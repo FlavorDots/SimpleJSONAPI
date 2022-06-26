9c09ccaaf715f1622c3267fe42c525058b8e91e6
Alain Mindana <alainmindana@gmail.com>
2022-06-26 20:57:23 +0800

    1st commit

---
+++ src/main/java/com/assignment/jsonbind/dto/SubjectIDSubjectCodeDTO.java
@@ -0,0 +1,23 @@
+package com.assignment.jsonbind.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class SubjectIDSubjectCodeDTO {
+
+    private String student_id;
+
+    private List<ClassDetailsDTO> class_details;
+
+    public List<ClassDetailsDTO> getClass_details() {
+        return class_details;
+    }
+}
